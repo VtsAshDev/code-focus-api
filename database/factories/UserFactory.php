1e8cd0ffdf94cf63f1a82540a7c01b6b25f1d22c
VtsAshDev <vitorgguimaraes56@gmail.com>
2026-06-06 00:02:47 -0300

    [feat]#User:Edita a migration da entidade usuário de acordo com a definição arquitetada e otimiza as factory do mesmo

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends Factory<User>
@@ -25,11 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'id' => $this->faker->uuid(),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'total_points' => 0,
         ];
     }
 
